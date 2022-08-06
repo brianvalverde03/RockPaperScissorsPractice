dfc35ef7a25eef4f7c2ebb86b5ae81d17f089ab7
Brian Valverde <207882@mtstmichael.org>
2022-08-05 21:55:05 -0400

    was able to get help menu, title screen, and title screen options to work, and was able to get game to boot to title screen

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,19 +1,49 @@
-import random, sys
+import random, sys, os
 
-print("Welcome to Rock, Paper, Scissors")
 
 wins = 0
 losses = 0
 ties = 0
 
-while True:
-    print('{}s Wins, {}s Losses, {}s Ties'.format(wins, losses, ties))
-    while True:
-        print('Enter your move: (r)ock, (p)aper, (s)cissors, (q)uit')
-        playerMove = input.lower()
-        if playerMove == 'q':
+def title_screen_selections():
+    option = input('> ')
+    if option.lower() == ("play") or option.lower() == ("p"):
+        start_game()
+    elif option.lower() == ("help") or option.lower() == ("h"):
+        help_menu()
+    elif option.lower() == ("quit") or option.lower() == ("q"):
+        sys.exit()
+    while option.lower() not in ['play', 'help', 'quit']:
+        print('Please enter a valid command.')
+        if option.lower() == ("play"):
+            start_game()
+        elif option.lower() == ("help"):
+            help_menu()
+        elif option.lower() == ("quit"):
             sys.exit()
-        if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
-            break
-        else:
-            print('Please ')
+
+def title_screen():
+    os.system('cls')
+    print('*************************************')
+    print('* Welcome to Rock, Paper, Scissors! *')
+    print('*            - Play -               *')
+    print('*            - Help -               *')
+    print('*            - Quit -               *')
+    print('*************************************')
+    title_screen_selections()
+
+def help_menu():
+    os.system('cls')
+    print('*************************************')
+    print('* Welcome to Rock, Paper, Scissors! *')
+    print('*************************************')
+    print('*  To play you much chose either    *')
+    print('*  rock, paper or scissors.         *')
+    print('*  You win when you have won two    *')
+    print('*  out of three.                    *')
+    print('*************************************')
+    title_screen_selections()
+
+
+
+title_screen()
